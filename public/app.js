9bef1e62d2151cd920b0284f2d1002966cc30957
Jose <gccinane@gmail.com>
2020-05-25 20:43:58 -0300

    fetch da api

--- public/app.js
+++ public/app.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', ()=> {
-  console.log('deus')
+  handleApi()
 })
+
+async function handleApi(){
+  const response = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo').then(response => response.json().then((data)=> {
+    console.log(data)
+  })
+)
+}
+
